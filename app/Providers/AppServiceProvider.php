0dd52b8ca1309d4f0d6075d4fd90e3a18cecf1cb
asaadoov <asaadballah@gmail.com>
2023-07-23 11:20:28 +0200

    vercel update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,6 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    
     /**
      * Register any application services.
      *
@@ -24,20 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (config('app.vercel.enabled')) {
-            $paths = [
-                '/tmp/framework/sessions',
-                '/tmp/framework/cache',
-                '/tmp/storage/bootstrap/cache',
-                '/tmp/storage/framework/cache',
-                config('view.compiled'),
-            ];
-    
-            foreach ($paths as $path) {
-                if (! is_dir($path)) {
-                    mkdir($path, 0755, true);
-                }
-            }      
-        }
+        //
     }
 }
