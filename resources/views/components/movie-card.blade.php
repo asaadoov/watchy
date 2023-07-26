c6fbfc2d0a9586dda650b56a3cd592ca22f1bb77
asaadoov <asaadballah@gmail.com>
2023-07-26 09:31:27 +0200

    test

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -1,6 +1,4 @@
 <div class="mt-8 ">
-	{{ dd($movie['id']) }}
-
 	<a href="{{ route('movie.item', $movie['id']) }}">
 		<img src="{{$movie['poster_path']}}" alt="{{ $movie['title'] }}" class="hover:opacity-75 transition ease-in-out duration-150">
 	</a>
